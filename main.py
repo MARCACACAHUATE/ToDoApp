e9a67b234a5c863ca399a16ed3a32fb8fa2fe2fc
Gustavo Luna <marcacacahuate@outlook.com>
2021-12-02 17:42:55 -0600

    Se me olvido mandar este pedo en el commit anterior

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+from flask import render_template
+
 from app import create_app
 
 app = create_app()
 
+todos = ["Caca", "Pipi", "Popo"]
 
-@app.route("/")
+@app.route("/", methods = ['POST', 'GET'])
 def home():
-    return "Que onda pinches perros"
+    context = {
+        "todos": todos
+    }
+
+    return render_template("home.html", **context)
 
 
 if __name__ == "__main__":
